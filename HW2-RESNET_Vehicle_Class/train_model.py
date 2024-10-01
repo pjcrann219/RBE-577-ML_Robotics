0d581dd51b75fdacd412a67b8a22d7dd636ff08b
Paul PC <pcrann@gmail.com>
2024-10-01 07:55:22 -0400

    update train_model

--- HW2-RESNET_Vehicle_Class/train_model.py
+++ HW2-RESNET_Vehicle_Class/train_model.py
@@ -19,7 +19,7 @@ model.fc = nn.Linear(model.fc.in_features, 10)
 
 # Hyper Parameters
 hyperParams = {
-    'batch_size': 7,
+    'batch_size': 32,
     'num_epochs': 100,
     'learning_rate': 0.001
 }
@@ -44,47 +44,66 @@ optimizer = optim.Adam(model.parameters(), lr=hyperParams['learning_rate'])
 
 writer = SummaryWriter(log_dir='runs/testing')
 
-for epoch in range(hyperParams['num_epochs']):
-    model.train()
-    train_avg_loss = 0.0
-    for batch_idx, (inputs, labels) in enumerate(train_loader):
-        inputs, labels = inputs.to(device), labels.to(device)
-
-        optimizer.zero_grad()
-
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
-
-        loss.backward()
-        optimizer.step()
-        
-        train_avg_loss += loss.item()
-
-    train_avg_loss = train_avg_loss / len(train_loader)
-    writer.add_scalar('Loss/train', train_avg_loss, epoch)
-
-    # print(f"Epoch [{epoch + 1}/{hyperParams['num_epochs']}], Loss: {running_loss / 10:.4f}")
-    running_loss = 0.0
-
-    model.eval()
-    test_avg_loss = 0.0
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for inputs, labels in test_loader:
+try:
+    for epoch in range(hyperParams['num_epochs']):
+        model.train()
+        train_avg_loss = 0.0
+        total = 0
+        correct = 0
+        for batch_idx, (inputs, labels) in enumerate(train_loader):
             inputs, labels = inputs.to(device), labels.to(device)
 
-            # Forward pass
+            optimizer.zero_grad()
+
             outputs = model(inputs)
             loss = criterion(outputs, labels)
-            test_avg_loss += loss.item()
+
+            loss.backward()
+            optimizer.step()
             
-            # Calculate accuracy
+            train_avg_loss += loss.item()
+
             _, predicted = torch.max(outputs, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-    test_avg_loss = test_avg_loss / len(test_loader)
-    writer.add_scalar('Loss/test', test_avg_loss, epoch)
-    accuracy = 100 * correct / total
-    print(f'Test Loss: {test_avg_loss:.4f}, Test Accuracy: {accuracy:.2f}%')
+        train_accuracy = 100 * correct / total
+        train_avg_loss = train_avg_loss / len(train_loader)
+        writer.add_scalar('Loss/train', train_avg_loss, epoch)
+        writer.add_scalar('Accuracy/train', train_accuracy, epoch)
+
+        # print(f"Epoch [{epoch + 1}/{hyperParams['num_epochs']}], Loss: {running_loss / 10:.4f}")
+        running_loss = 0.0
+
+        model.eval()
+        test_avg_loss = 0.0
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for inputs, labels in test_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
+
+                # Forward pass
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                test_avg_loss += loss.item()
+                
+                # Calculate accuracy
+                _, predicted = torch.max(outputs, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+        test_avg_loss = test_avg_loss / len(test_loader)
+        writer.add_scalar('Loss/test', test_avg_loss, epoch)
+        test_accuracy = 100 * correct / total
+        writer.add_scalar('Accuracy/test', test_accuracy, epoch)
+        
+        print(f'Test Loss: {test_avg_loss:.4f}, Test Accuracy: {test_accuracy:.2f}%')
+except KeyboardInterrupt:
+    print('\nTraining Interrupted')
+
+# Save Model
+model_name = input('Enter name to save model, enter otherwise: ')
+if model_name:
+    torch.save(model.state_dict, 'models/' + model_name + '.pth')
+    print(f"Model saved in models/{model_name}.pth")
